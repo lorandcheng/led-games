627c5a21bc677608ef04f7a0c42ed1d378ea6c4a
bcinbis <baran.cinbis1@gmail.com>
2020-12-02 21:51:06 -0700

    selectLocation:

--- src/games/checkers.py
+++ src/games/checkers.py
@@ -18,11 +18,40 @@ class Checkers():
         self.blackCounter = 12
         self.yourTurn = 1
 
-    def selectLocation(self):
+    def selectLocation(self, locations):
         #select a position of tuple from list of tuples
 
 
-        return location
+        
+        length  = locations.len()
+        inp = ""
+        while inp != "e":
+            inp = input()
+            index = 0
+            if inp == 'a':
+                index -= 1
+            elif inp == 'd':
+                index += 1
+            else:
+                print("invalid input")
+            if index >= length:
+                index = 0
+            elif index <= -1:
+                index = length-1
+
+            row = locations[index[0]]
+            column = locations[index[1]]
+            
+            for r,c in locations:
+                self.BOARD[r][c] == "*"
+            self.BOARD[row][column] == "X"
+            self.printBoard()
+            selection = (row, column)
+
+        for r,c in locations:
+            self.BOARD[r][c] == "0"
+
+        return selection
 
     def findPieces(self):
         val = self.color
@@ -35,6 +64,7 @@ class Checkers():
 
     def findMoves(self):
         # return coordinates of possible final moves
+        
         pass
 
     def makeMove(self, location, final):
