c3a313822717811098d9bd64c444abd0dc865b40
bcinbis <baran.cinbis1@gmail.com>
2020-12-03 19:00:41 -0700

    checkers game working on terminal

--- src/games/checkers.py
+++ src/games/checkers.py
@@ -12,6 +12,7 @@ import sys
 class Checkers():
     def __init__(self):
         # -1 red,  0 empty,  1 black
+
         self.BOARD = [
             [ 0, -1,  0, -1,  0, -1,  0, -1], 
             [-1,  0, -1,  0, -1,  0, -1,  0], 
@@ -22,10 +23,12 @@ class Checkers():
             [ 0,  1,  0,  1,  0,  1,  0,  1], 
             [ 1,  0,  1,  0,  1,  0,  1,  0], 
         ]
+ 
+
         # self.BOARD = [
-        #     [ 0, -1,  0, -1,  0, -1,  0, -1], 
-        #     [-1,  0, -1,  0, -1,  0, -1,  0], 
-        #     [ 0,  0,  0, -1,  0,  0,  0, -1], 
+        #     [ 0,  0,  0, -1,  0, -1,  0, -1], 
+        #     [-1,  0,  1,  0, -1,  0, -1,  0], 
+        #     [ 0, -1,  0, -1,  0,  0,  0, -1], 
         #     [ 0,  0, -1,  0, -1,  0,  0,  0], 
         #     [ 0,  0,  0,  2,  0,  0,  0,  0], 
         #     [ 1,  0, -1,  0, -1,  0,  1,  0], 
@@ -76,18 +79,21 @@ class Checkers():
             tempBoard[row][column] = "X"
             self.printBoard(tempBoard)
             selection = (row, column)
-        tempBoard = copy.copy(self.BOARD) 
+        tempBoard = copy.deepcopy(self.BOARD) 
         tempBoard[row][column] = "X"
         self.printBoard(tempBoard)
         return selection
 
-    def canJump(self, row, col):
+    def canJump(self, location):
+        
+        row, col = location
         locs= []
         a = -1*self.color
         b = 1
         c = -1
         d = self.color
         checks = [(a,b),(a,c)]
+        self.printBoard(self.BOARD)
         if abs(self.BOARD[row][col]) == 2:
             checks.append((d,b))
             checks.append((d,c))
@@ -117,7 +123,7 @@ class Checkers():
         for r in range(8):
             for c in range(8):
                 if val * self.BOARD[r][c] > 0:
-                    if self.canJump(r,c):
+                    if self.canJump((r,c)):
                         locations.append((r,c))
 
         if len(locations):
@@ -127,7 +133,7 @@ class Checkers():
             for c in range(8):
                 if val * self.BOARD[r][c] > 0:
                     # TODO if findMoves((r,c)) != 0:
-                    if self.findMoves(r,c):
+                    if self.findMoves((r,c)):
                         locations.append((r,c))
         return locations
 
@@ -143,7 +149,7 @@ class Checkers():
             return True
         return False
 
-    def findMoves(self, row, col):
+    def findMoves(self, location):
         '''
         Summary: finds all possible moves, jumping or non-jumping, for a given piece
 
@@ -154,7 +160,7 @@ class Checkers():
             locs (list of tuples): coordinates of possible moves
             -1: if there are no possible moves for the given piece
         '''
-
+        row, col = location
         locs = []
 
         if self.isOnBoard(row-self.color,col+1) and self.isEmpty(row-self.color,col+1):
@@ -187,11 +193,17 @@ class Checkers():
         rowF, colF = final # final row, col
         piece = self.BOARD[rowI][colI]
         if (abs(rowI - rowF)) > 1: # if a piece was hopped, remove piece and decrement counter
-            self.BOARD[(rowI+rowF)/2][(colI+colF)/2] = 0
+            self.BOARD[int((rowI+rowF)/2)][int((colI+colF)/2)] = 0
             if self.color == 1:
                 self.redCounter -= 1
             else:
                 self.blackCounter -= 1
+        
+        if rowF == 0 and piece == 1:
+            piece = 2
+        elif rowF == 7 and piece == -1:
+            piece = -2
+
         # update board
         self.BOARD[final[0]][final[1]] = piece
         self.BOARD[location[0]][location[1]] = 0
@@ -254,6 +266,7 @@ class Checkers():
                     row += '   |'
             print(row)
             print('+---+---+---+---+---+---+---+---+')
+        print(str(self.color) + " Black: " + str(self.blackCounter) + " Red: " + str(self.redCounter) )
     
 
 
@@ -262,16 +275,29 @@ class Checkers():
         while self.redCounter > 0 and self.blackCounter > 0:
             pieces = self.findPieces()
             pieceLocation = self.selectLocation(pieces)
-
+            
+            hasJumped = 0
+            hasMoved = 0
+            
             while True:
-                possibleMoves = self.findMoves(pieceLocation)
-                if possibleMoves == -1:
+                possibleMoves = self.canJump(pieceLocation)
+                if hasJumped == 0:
+                    if not possibleMoves:
+                        possibleMoves = self.findMoves(pieceLocation)
+                        hasMoved = 1
+                        hasJumped = 0
+                    else:
+                        hasJumped = 1
+                if possibleMoves == 0:
                     break
                 moveLocation = self.selectLocation(possibleMoves)
                 self.makeMove(pieceLocation, moveLocation)
                 pieceLocation = moveLocation
+                if hasMoved:
+                    break
 
             self.color *= -1
+            print("switch turns")
             
         if self.color == 1 and self.blackCounter > 0:
             self.gameOver(True)
@@ -282,4 +308,4 @@ class Checkers():
 
 if __name__ == '__main__':
     game = Checkers()
-    print(game.findMoves(1,6))
+    game.main()
