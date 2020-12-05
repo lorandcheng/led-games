ef545242d04c32e25d367f41502c783c0e87d59c
Lorand Cheng <lfcheng@usc.edu>
2020-12-05 10:57:21 -0800

    mqtt client steps laid out

--- client/src/mqtt.py
+++ client/src/mqtt.py
@@ -1,3 +1,62 @@
+"""
+MQTT CLIENT STEPS
+=================
+
+SETUP USERNAME
+--------------
+1. SUB "ledGames/users"
+2. Generate a username
+3. Check username against existing usernames by PUB "ledGames/user/status" 
+    message = (name, code)
+    name: generated username
+    code: 1 to add, 0 to remove username
+4. Listen for a response on "ledGames/users"
+    response = (name, code)
+    name: username that the response is for
+    code: 1 for sucess, 0 for failure (username taken or other failure)
+
+JOIN LOBBY AND SELECT OPPONENT
+------------------------------
+5. SUB "ledGames/lobby" and "ledGames/<username>/#", add callbacks for lobby, request, and play
+6. Join lobby with PUB "ledGames/lobby/status"
+    message = (name, game)
+    name: user updating status
+    game: game user wants to play, or 0 to leave lobby
+7. Choose an opponent from the lobby
+8. Send the opponent a game request with PUB "ledGames/<opponent>/request"
+    message = (name, request)
+    name: username
+    request: 1 to request or confirm, 0 to deny
+9. Listen for a response on "ledGames/<username>/request"
+    response = (opponent, request)
+    opponent: opponent's username
+    request: 1 to request or confirm, 0 to deny
+10. If the request was denied, repeat from step 7, otherwise leave lobby with PUB "ledGames/lobby/status"
+    message = (name, game)
+    name: user updating status
+    game: game user wants to play, or 0 to leave lobby
+
+PLAY GAME
+---------
+Each player listens on "ledGames/<ownUsername>/play" and sends on "ledGames/<opponentsUsername>/play"
+
+11. The player who initiated the connnection picks a random color, then sends the color assignments across
+    message = [(name, color),(name, color)]
+    name: one of the two usernames
+    color: the corresponding color (varies by game)
+        Checkers: 1 (black) or -1 (red)
+
+12. After the first player makes a move, they send the game info
+    message = (board, scores)
+    board: setup of the board at the end of player's turn
+    scores: scores at the end of player's turn
+
+...
+
+
+"""
+
+
 import paho.mqtt.client as mqtt
 import time
 
@@ -45,8 +104,6 @@ def onMessage(client, userdata, msg):
 
 def onDisconnect(client, useradata, rc):
     print("disconnecting")
-    client.publish("ledGames/users/status", f'lorand, 0')
-    print("published")
 
 def mqttInit():   
     client = mqtt.Client()
@@ -55,16 +112,11 @@ def mqttInit():
     client.on_disconnect = onDisconnect
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
-    inputName(client)
-    time.sleep(3)
-    client.disconnect()
-    client.loop_stop()
-    time.sleep(3)
     return client
     
 
 if __name__ == '__main__':
     client =  mqttInit()
-
+    inputName(client)
         
 
