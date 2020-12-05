ed8c37eb5bd72841df4b4b82b9eb37f9d7e2da82
Lorand Cheng <lfcheng@usc.edu>
2020-12-04 17:30:08 -0800

    mqtt fixes

--- src/mqtt.py
+++ src/mqtt.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
-name = ""
+import time
 
+name = ""
 
 def parseMessage(message):
     parsedMessage = tuple(map(str, message.split(", ")))
@@ -33,31 +34,24 @@ def onConnect(client, userdata, flags, rc):
     print("Connection returned " + str(rc))
     client.subscribe("ledGames/users")
     client.message_callback_add("ledGames/users", verifyName)
-
     inputName(client)
 
 
 
 def onMessage(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+    print("onMessage")
+    #print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 
-def mqttInit():
-    print("b")
-    
+def mqttInit():   
     client = mqtt.Client()
     client.on_connect = onConnect
     client.on_message = onMessage
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
+    while True:
+        time.sleep(3)
 
-#if __name__ == '__main__':
-    # mqttInit()
-
-    # client = mqtt.Client()
-    # client.on_connect = onConnect
-    # client.on_message = onMessage
-    # client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    # client.loop_start()
+if __name__ == '__main__':
+    mqttInit()
     
-    #print(parseMessage("evan, poopoo, stinky")[1:3])
